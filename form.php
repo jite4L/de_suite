cc81e3a0555a04540c86a12a65be2152ca33219a
jite4L <onajite211@gmail.com>
2023-09-05 23:04:57 +0100

    new

--- form.php
+++ form.php
@@ -21,7 +21,10 @@ if (isset($_REQUEST["submit"])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="./css/newf.css">
+    
+    <!-- <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="./css/form.css">
+
 
 </head>
 
@@ -29,12 +32,12 @@ if (isset($_REQUEST["submit"])) {
 
     <div class="login-form">
         <form>
-            <h1>Login</h1>
-            <input type="text" class="input-place" id="firstName" name="firstName" placeholder="First Name" required><br><br>
+            <h1>Details</h1>
+            <input type="text" class="input-place " id="firstName" name="firstName" placeholder="First Name" required><br><br>
             <input type="text" class="input-place" id="lastName" name="lastName" placeholder="Last Name" required><br><br>
             <input type="email" class="input-place" id="email" name="email" placeholder="Email" required><br><br>
             <input type="number" class="input-place" id="phoneNumber" name="phoneNumber" placeholder="Phone Number" required><br><br><br>
-            <button class="login-button" type="submit" name="submit">Login</button>
+            <button class="login-button" type="submit" name="submit">Send</button>
 
         </form>
     </div>
