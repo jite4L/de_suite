43f17e0c62f5df10e512ced27a5193b770beada3
jite4L <onajite211@gmail.com>
2023-09-08 00:18:43 +0100

    c

--- form.php
+++ form.php
@@ -29,25 +29,25 @@ if (isset($_REQUEST["submit"])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-    <!-- <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css"> -->
-    <link rel="stylesheet" type="text/css" href="./css/form.css">
+    <link rel="stylesheet"  href="./css/form.css">
+    <!-- <link rel="stylesheet"  href="./css/bootstrap.min.css"> -->
 
 
 </head>
 
 <body>
-    <header>
-        <nav>
+    <!-- <header> -->
+        <!-- <nav> -->
 
             <!-- <a href="#"><img class="logo" src="./images/Group 29.png" alt=""></a> -->
-        </nav>
+        <!-- </nav> -->
 
         <div class="hero-section">
             <div class="login-form">
-                <form action="contact_us.php" method="POST">
+                <form>
                     <img src="./images/Group 29.png" class="img-size" alt="">
                     <h1>Details</h1>
-                    <input type="text" class="input-place " id="firstName" name="firstName" placeholder="First Name" required><br><br>
+                    <input type="text" class="input-place focus-ring " id="firstName" name="firstName" placeholder="First Name" required><br><br>
                     <input type="text" class="input-place" id="lastName" name="lastName" placeholder="Last Name" required><br><br>
                     <input type="email" class="input-place" id="email" name="email" placeholder="Email" required><br><br>
                     <input type="number" class="input-place" id="phoneNumber" name="phoneNumber" placeholder="Phone Number" required><br><br><br>
@@ -56,7 +56,7 @@ if (isset($_REQUEST["submit"])) {
                 </form>
             </div>
         </div>
-    </header>
+    <!-- </header> -->
 
 
 
