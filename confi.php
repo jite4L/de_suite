3c83c5c19ef72df6a9484b65ec7588e2eb26f174
jite4L <onajite211@gmail.com>
2023-09-23 00:06:24 +0100

    edited the index.php file

--- confi.php
+++ confi.php
@@ -1,9 +1,9 @@
 <?php
 
-// define('DB_SERVER','localhost');
-// define('DB_USERNAME','root');
-// define('DB_PASSWORD','');
-// define('DB_NAME','hotel');
+define('DB_SERVER','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','hotel');
 
-// $connection  = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+$connection  = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
